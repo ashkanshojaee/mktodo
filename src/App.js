00976883ed10fe110a276c5c61e0f5b85dfcc547
unknown <ashkanshojaee48@gmail.com>
2020-11-24 13:04:31 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, {Component} from 'react';
-import {observer} from 'mobx-react'
-import './App.css';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import "./App.css";
 import todoStore from "./stores/TodoStore";
+import TodoEntry from "./components/TodoEntry"
+import TodoItems from "./components/TodoItems";
+import TodoFooter from "./components/TodoFooter"
 
 @observer
 class App extends Component {
-    render() {
-        return [
-            <div id="todoapp" className="todoapp">
-                {/* your code here */}
-                <h1>Worked :)</h1>
-            </div>,
-            <footer className="info">
-                <p>Written by <a href="#">Your Name</a></p>
-                <p>Project of <a href="https://maktabkhooneh.org">Maktabkhooneh</a></p>
-            </footer>
-        ]
-    }
+  render() {
+    return (
+        <div id="todoapp" className="todoapp">
+           <TodoEntry/>
+            <TodoItems />
+            <TodoFooter/>
+        </div>
+    )
+  }
 }
-
 
 export default App;
